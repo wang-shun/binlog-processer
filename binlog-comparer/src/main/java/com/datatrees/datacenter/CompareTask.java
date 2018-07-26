93a4cd97c4565e7c9a79bb99416a4ec3628087f1
personalc <personalc@personalcdeMacBook-Pro.local>
2018-07-26 14:00:51 +0800

    fix conflict

--- binlog-comparer/src/main/java/com/datatrees/datacenter/CompareTask.java
+++ binlog-comparer/src/main/java/com/datatrees/datacenter/CompareTask.java
@@ -1,6 +1,6 @@
 package com.datatrees.datacenter;
 
-import com.datatrees.datacenter.compare.DataCompare;
+import com.datatrees.datacenter.compare.BaseDataCompare;
 import com.datatrees.datacenter.compare.TiDBCompare;
 import com.datatrees.datacenter.core.task.TaskRunner;
 import com.datatrees.datacenter.resolver.TaskProcessor;
@@ -8,19 +8,19 @@ import com.datatrees.datacenter.resolver.TaskProcessorListner;
 
 public class CompareTask implements TaskRunner {
 
-  public void startCheck() {
-    TaskProcessor.defaultProcessor().setTopic("local_topic").registerListner(
-      new TaskProcessorListner() {
-        @Override
-        public void onMessageReceived(String desc) {
-          DataCompare compare = new TiDBCompare();
-          compare.binLogCompare(desc);
-        }
-      }).process();
-  }
+    public void startCheck() {
+        TaskProcessor.defaultProcessor().setTopic("local_topic").registerListner(
+                new TaskProcessorListner() {
+                    @Override
+                    public void onMessageReceived(String desc) {
+                        BaseDataCompare compare = new TiDBCompare();
+                        compare.binLogCompare(desc, "create");
+                    }
+                }).process();
+    }
 
-  @Override
-  public void process() {
-    startCheck();
-  }
+    @Override
+    public void process() {
+        startCheck();
+    }
 }
