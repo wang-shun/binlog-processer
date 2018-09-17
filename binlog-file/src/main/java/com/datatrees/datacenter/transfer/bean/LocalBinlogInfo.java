911b9f98d567f5038ecdfb46d96114079f1331df
personalc <personalc@personalcdeMacBook-Pro.local>
2018-09-17 14:02:11 +0800

    test

--- binlog-file/src/main/java/com/datatrees/datacenter/transfer/bean/LocalBinlogInfo.java
+++ binlog-file/src/main/java/com/datatrees/datacenter/transfer/bean/LocalBinlogInfo.java
@@ -3,7 +3,7 @@ package com.datatrees.datacenter.transfer.bean;
 public class LocalBinlogInfo {
     public static final String dataBase="binlog";
     public static final String lastDownloadFileTable="t_binlog_down_last_file";
-    public static final String binlogDownloadRecordTable="t_binlog_record_copy";
+    public static final String binlogDownloadRecordTable="t_binlog_record";
     public static final String id="id";
     public static final String dbInstance="db_instance";
     public static final String downloadIp="download_ip";
