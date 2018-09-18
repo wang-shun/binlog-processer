132509b17e627f98c0625d3734cfca0a21d53362
zxding1986 <zxding1986@zxding1986deMacBook-Pro.local>
2018-09-18 21:22:33 +0800

    prometheus 修改端口

--- binlog-core/src/main/java/com/datatrees/datacenter/core/utility/PrometheusMetrics.java
+++ binlog-core/src/main/java/com/datatrees/datacenter/core/utility/PrometheusMetrics.java
@@ -1,6 +1,7 @@
 package com.datatrees.datacenter.core.utility;
 
 import io.prometheus.client.exporter.HTTPServer;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,6 +9,13 @@ public class PrometheusMetrics {
 
   private static Integer PORT = 9097;
   private static Logger logger = LoggerFactory.getLogger(PrometheusMetrics.class);
+
+  static {
+    PORT =
+      StringUtils.isBlank(PropertiesUtility.defaultProperties().getProperty("PROMETHEUS.PORT"))
+        ? 9097 :
+        Integer.valueOf(PropertiesUtility.defaultProperties().getProperty("PROMETHEUS.PORT"));
+  }
 
   public void start() {
     try {
