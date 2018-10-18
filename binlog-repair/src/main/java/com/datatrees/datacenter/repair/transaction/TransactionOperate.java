00dc031a95cd1a57693fdee88e3d3d76426f5299
personalc <personalc@personalcdeMacBook-Pro.local>
2018-10-18 11:08:00 +0800

    read metestore.uri from config file

--- binlog-repair/src/main/java/com/datatrees/datacenter/repair/transaction/TransactionOperate.java
+++ binlog-repair/src/main/java/com/datatrees/datacenter/repair/transaction/TransactionOperate.java
@@ -1,5 +1,6 @@
 package com.datatrees.datacenter.repair.transaction;
 
+import com.datatrees.datacenter.core.utility.PropertiesUtility;
 import com.datatrees.datacenter.operate.OperateType;
 import com.google.common.collect.Lists;
 import com.tree.finance.bigdata.hive.streaming.utils.InsertMutation;
@@ -11,6 +12,7 @@ import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hive.conf.HiveConf;
 
 import java.util.List;
+import java.util.Properties;
 
 /**
  * @author personalc
@@ -19,11 +21,9 @@ public class TransactionOperate {
     private static HiveConf hiveConf = new HiveConf();
 
     private static Configuration configuration = HBaseConfiguration.create();
-
-    // FIXME: 2018/10/9 从配置文件中读取
-    private static final String METASTORE_URIS = "thrift://hadoop3:9083";
-
-    private static final int TRANSACTION_SIZE = 10000;
+    private static Properties properties = PropertiesUtility.defaultProperties();
+    private static final String META_STORE_URIS = properties.getProperty("hive.metastore.uri");
+    private static final int TRANSACTION_SIZE = Integer.valueOf(properties.getProperty("id.list.max"));
 
     public static void repairTransaction(String dataBase, String tableName, String hivePartition, List<String> hivePartitions, String operate, Schema schema, List<GenericData.Record> genericRecordList) {
         List<List<GenericData.Record>> splitList = Lists.partition(genericRecordList, TRANSACTION_SIZE);
@@ -31,7 +31,7 @@ public class TransactionOperate {
             InsertMutation mutation = null;
             try {
                 for (List<GenericData.Record> subList : splitList) {
-                    mutation = new InsertMutation(dataBase, tableName, hivePartition, hivePartitions, METASTORE_URIS, configuration);
+                    mutation = new InsertMutation(dataBase, tableName, hivePartition, hivePartitions, META_STORE_URIS, configuration);
                     mutation.beginFixTransaction(schema, hiveConf);
                     for (GenericData.Record record : subList) {
                         mutation.insert(record);
@@ -48,7 +48,7 @@ public class TransactionOperate {
             UpdateMutation mutation = null;
             try {
                 for (List<GenericData.Record> subList : splitList) {
-                    mutation = new UpdateMutation(dataBase, tableName, hivePartition, hivePartitions, METASTORE_URIS, configuration, true);
+                    mutation = new UpdateMutation(dataBase, tableName, hivePartition, hivePartitions, META_STORE_URIS, configuration, true);
                     mutation.beginFixTransaction(schema, hiveConf);
                     for (GenericData.Record record : subList) {
                         mutation.update(record);
